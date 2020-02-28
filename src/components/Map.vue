907ba77b3e23458080f9a110b286e0a9c9d6e604
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2020-02-28 12:04:18 +0100

    eslint added

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,10 +1,38 @@
 <template>
-  <svg id="map"/>
+  <svg id="map-chart" />
 </template>
 
 <script>
+import * as d3nic from 'd3nic'
+
 export default {
   name: 'Map',
-  props: {}
+  props: {
+    geoJson: Object
+  },
+  data () {
+    return {
+      chart: null,
+      geoRegions: null
+    }
+  },
+  mounted () {
+
+  },
+  created () {
+    this.createComponents()
+    this.createChart()
+  },
+  methods: {
+    createComponents () {
+      this.geoRegions = d3nic.geoRegions()
+    },
+    createChart () {
+      this.chart = d3nic.geoChart()
+        .selector('#map-chart')
+        .fnKey(d => d.concept)
+        .components([this.geoRegions])
+    }
+  }
 }
 </script>
