a4e530a9f3cd35a4d63371fc72a4b247686005e4
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2020-02-28 13:34:20 +0100

    map done

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,9 +1,15 @@
 <template>
-  <svg id="map-chart" />
+  <div>
+    <svg id="map-chart" />
+  </div>
 </template>
 
 <script>
 import * as d3nic from 'd3nic'
+import { zoom } from 'd3-zoom'
+import { select, event } from 'd3-selection'
+
+import world from '../assets/world.json'
 
 export default {
   name: 'Map',
@@ -17,7 +23,12 @@ export default {
     }
   },
   mounted () {
+    const fnZoom = zoom().scaleExtent([1, 8]).on('zoom', this.zoomed)
+    select('svg#map-chart').call(fnZoom)
 
+    this.chart
+      .data(world.features)
+      .draw()
   },
   created () {
     this.createComponents()
@@ -26,12 +37,18 @@ export default {
   methods: {
     createComponents () {
       this.geoRegions = d3nic.geoRegions()
+        .fnValue(d => d.geometry)
+        .fnFill(d => '#000')
     },
     createChart () {
       this.chart = d3nic.geoChart()
         .selector('#map-chart')
-        .fnKey(d => d.concept)
+        .fnKey(d => d.properties.adm0_a3)
+        .size({ width: 800, height: 800 })
         .components([this.geoRegions])
+    },
+    zoomed () {
+      this.geoRegions.join().attr('transform', event.transform)
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 Vue.config.productionTip = false
+
+// Vue.component('ChartTooltip', ChartTooltip)
 
 new Vue({
   render: h => h(App)
