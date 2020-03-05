9253dd687ab462b6ec3e5af22709dff7ad255c47
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2020-03-05 01:03:55 +0100

    transform is good

--- src/components/MapChart.vue
+++ src/components/MapChart.vue
@@ -26,7 +26,7 @@
 <script>
 import * as d3nic from 'd3nic'
 
-import { zoom, zoomIdentity, zoomTransform } from 'd3-zoom'
+import { zoom, zoomIdentity } from 'd3-zoom'
 import { select, event } from 'd3-selection'
 
 import { mapMutations, mapGetters } from 'vuex'
