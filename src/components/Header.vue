8c5600a09e2ef3a3c2e3bab38d27f6185f2c3384
Nicola Castaldo <ncastaldo@MBP-di-Nicola.fritz.box>
2021-02-12 10:07:19 +0100

    new d3nic version

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,33 +1,18 @@
 <template>
-  <v-app-bar
-    app
-    height="60"
-  >
-    <v-app-bar-nav-icon
-      v-if="false"
-      @click="toggleDrawer"
-    />
-    <v-toolbar-title
-      class="ml-1 text-left headline"
-    >
-      <a
-        class="pr-2"
-        :style="style"
-        href="/#/"
-      >
-        covid19obs
-      </a>
+  <v-app-bar app height="60">
+    <v-app-bar-nav-icon v-if="false" @click="toggleDrawer" />
+    <v-toolbar-title class="ml-1 text-left headline">
+      <a class="pr-2" :style="style" href="/#/"> covid19obs </a>
       <span
         v-if="!['xs', 'sm'].includes(breakpoint)"
-        class="font-weight-light pl-2 "
-        :style="{ 'font-size': '18px'}"
+        class="font-weight-light pl-2"
+        :style="{ 'font-size': '18px' }"
       >
         Covid19 Infodemics Observatory
       </span>
     </v-toolbar-title>
     <v-spacer />
     <v-img
-
       v-if="false && breakpoint !== 'xs'"
       class="mx-2"
       :src="imgSrc"
@@ -39,31 +24,31 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions } from "vuex";
 
 export default {
-  name: 'Header',
-  data () {
+  name: "Header",
+  data() {
     return {
-      imgSrc: require('../assets/img/FBK_WHO.svg')
-    }
+      imgSrc: require("../assets/img/FBK_WHO.svg"),
+    };
   },
   computed: {
-    breakpoint () {
-      return this.$vuetify.breakpoint.name
+    breakpoint() {
+      return this.$vuetify.breakpoint.name;
     },
-    style () {
+    style() {
       return {
-        'font-size': this.breakpoint === 'xs' ? '20px' : '24px',
-        color: '#2877b8',
-        'text-decoration': 'none'
-      }
-    }
+        "font-size": this.breakpoint === "xs" ? "20px" : "24px",
+        color: "#2877b8",
+        "text-decoration": "none",
+      };
+    },
   },
   methods: {
     ...mapActions({
-      toggleDrawer: 'view/toggleDrawer'
-    })
-  }
-}
+      toggleDrawer: "view/toggleDrawer",
+    }),
+  },
+};
 </script>
